437f0f6fb4e903321d6cf53fe72bf7bf215896ae
Roman Shcherbinin <idcgeneral@mail.ru>
2022-01-22 15:15:51 +0300

    second commit

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 
   const sliderItems = document.querySelectorAll('.slider__item');
   const sliderDots = document.querySelectorAll('.slider__dot');
@@ -6,28 +6,30 @@
   const nextBtn = document.querySelector('.slider__arrow_next');
   const prevBtn = document.querySelector('.slider__arrow_prev');
 
-  let position = 0;
   
-  sliderDots[position].classList.add('slider__dot_active');
-  
-  const changeSlide = function(position) {
+  const changeSlide = function (index) {
     sliderItems.forEach(item => item.classList.remove('slider__item_active'));
     sliderDots.forEach(item => item.classList.remove('slider__dot_active'));
-
-    sliderItems[position].classList.add('slider__item_active');
-    sliderDots[position].classList.add('slider__dot_active');
+    
+    sliderItems[index].classList.add('slider__item_active');
+    sliderDots[index].classList.add('slider__dot_active');
+    
   };
   
+  changeSlide(Array.from(sliderItems).findIndex(item => item.classList.contains('slider__item_active')));
+  
   nextBtn.addEventListener('click', () => {
-    position++;
-    if (position > 4) position = 0;
-    changeSlide(position);
+    let activeIndex = Array.from(sliderItems).findIndex(item => item.classList.contains('slider__item_active'));
+    let index = activeIndex + 1;
+    if (index > sliderItems.length - 1) index = 0;
+    changeSlide(index);
   });
 
   prevBtn.addEventListener('click', () => {
-    position--;
-    if (position < 0) position = 4;
-    changeSlide(position);
+    let activeIndex = Array.from(sliderItems).findIndex(item => item.classList.contains('slider__item_active'));
+    let index = activeIndex - 1;
+    if (index < 0) index = sliderItems.length - 1;
+    changeSlide(index);
   });
 
   sliderDots.forEach((dot, index) => {
