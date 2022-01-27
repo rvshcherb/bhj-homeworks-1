f6e1427bd99dde93fcf5136be6984e58ff690858
Roman Shcherbinin <idcgeneral@mail.ru>
2022-01-27 12:20:59 +0300

    HW-DOM Task-1 Tests Removed

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -2,24 +2,15 @@
   const elementsToShow = document.querySelectorAll('.reveal');
 
   const checkIfVisible = function() {
-    // elementsToShow.forEach(element => console.log(element.getBoundingClientRect()));
-    
-    elementsToShow.forEach((element, index) => {
+    elementsToShow.forEach(element => {
       let elemDimension = element.getBoundingClientRect();
-      //console.log(elemDimension);
       if(window.innerHeight - elemDimension.top > 0 && elemDimension.bottom > 0) {
         if(!element.classList.contains('reveal_active')) element.classList.add('reveal_active');
-        console.log(`element ${index} to show`);
       } else {
         if(element.classList.contains('reveal_active')) element.classList.remove('reveal_active');
-        console.log(`element ${index} to hide`);
       }
     });
-
-
-    //console.log(window.innerHeight);
   };
-  window.addEventListener('scroll', checkIfVisible);
 
-  //elementsToShow.forEach(element => element.addEventListener('scroll', checkIfVisible));
+  window.addEventListener('scroll', checkIfVisible);
 })();
