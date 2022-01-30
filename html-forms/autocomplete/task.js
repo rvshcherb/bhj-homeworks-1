0514653748801b87a1554e81308e935115703fac
Roman Shcherbinin <idcgeneral@mail.ru>
2022-01-30 18:05:20 +0300

    HW-html-forms Task-3 Initial Commit

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,25 +68,15 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
 
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
-      }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+      const optionsArr = Array.from(this.input.options)
+        .filter(word => word.text.includes(text))
+        .map(word => ({
+          text: word.text,
+          value: word.value
+        }));
+
+    return optionsArr;
   }
 }
 
